4649392d1e822c246b31d8a8f51b90b002ae4cf2
g0ofycat <g0ofycatbusiness@gmail.com>
2026-01-12 17:31:40 -0500

    benchmarking

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,31 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <chrono>
+#include <benchmark/benchmark.h>
 
 #include "../src/lexer/lexer.hpp"
-
 #include "../src/parser/parser.hpp"
 
-int main() {
-    Lexer math_lexer(R"(\int_{\alpha}^{\beta} f'(x) \, dx = f(\beta) - f(\alpha))");
+static void BM_LexerTokenization(benchmark::State& state) {
+    std::string input = R"(\int_{\alpha}^{\beta} f'(x) \, dx = f(\beta) - f(\alpha))";
 
-    auto start = std::chrono::high_resolution_clock::now();
-    auto tokens = math_lexer.tokenize();
-    auto end = std::chrono::high_resolution_clock::now();
+    for (auto _ : state) {
+        Lexer math_lexer(input);
+        auto tokens = math_lexer.tokenize();
 
-    double elapsed_ms = std::chrono::duration<double, std::milli>(end - start).count();
-
-    std::cout << "Tokenization Speed: " << elapsed_ms << " ms\n";
-
-    std::cout << "Tokens (" << tokens.size() << "):\n";
-
-    for (const auto& tok : tokens) {
-        std::cout << "  "
-                  <<  static_cast<int>(tok.Type)
-                  << "  \"" << tok.Value << "\""
-                  << "  @" << tok.line << ":" << tok.column
-                  << "\n";
+        benchmark::DoNotOptimize(tokens);
     }
-
-    return 0;
 }
+
+BENCHMARK(BM_LexerTokenization);
+
+BENCHMARK_MAIN();
